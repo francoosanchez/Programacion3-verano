d9af9c9ec8baa4f0a9ef0d3fc4ade2dfd48a7119
francoosanchez <francosanchezaguero@gmail.com>
2025-01-28 16:52:01 -0300

    ej3 clase 1 - consigna

---
+++ Ejercicios-Clases/src/clase1/actividad3/ej3.java
@@ -0,0 +1,15 @@
+package clase1.actividad3;
+
+/*
+    Actividad 3: Calculadora Básica
+    
+    Objetivo: Practicar estructuras de control y entrada de datos.
+    Enunciado: Escribe un programa que:
+    Solicite dos números al usuario.
+    Solicite una operación matemática: suma, resta, multiplicación o división.
+    Realice la operación seleccionada y muestre el resultado.
+ */
+
+public class ej3 {
+    
+}
