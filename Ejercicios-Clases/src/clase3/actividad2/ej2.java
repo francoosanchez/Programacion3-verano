2c3eeed18234e719c5e14397ed071b96165cadf0
francoosanchez <francosanchezaguero@gmail.com>
2025-01-29 19:49:00 -0300

    clase 3 - ej2

---
+++ Ejercicios-Clases/src/clase3/actividad2/ej2.java
@@ -0,0 +1,39 @@
+package clase3.actividad2;
+
+public class ej2 {
+
+    // Método para realizar la búsqueda binaria
+    public static int busquedaBinaria(int[] arreglo, int objetivo) {
+        int izquierda = 0;
+        int derecha = arreglo.length - 1;
+
+        while (izquierda <= derecha) {
+            int medio = izquierda + (derecha - izquierda) / 2;
+
+            // Verificar si el objetivo está en el medio
+            if (arreglo[medio] == objetivo) {
+                return medio; // Retorna el índice del objetivo
+            }
+
+            // Si el objetivo es mayor, ignorar la mitad izquierda
+            if (arreglo[medio] < objetivo) {
+                izquierda = medio + 1;
+            } 
+            // Si el objetivo es menor, ignorar la mitad derecha
+            else {
+                derecha = medio - 1;
+            }
+        }
+
+        // Si no se encuentra el objetivo, retorna -1
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arreglo = {1, 3, 5, 7, 9, 11, 13};
+        int objetivo = 7;
+        int resultado = busquedaBinaria(arreglo, objetivo);
+        System.out.println("El indice donde se encuentra el objetivo es: " + resultado);
+
+    }
+}
