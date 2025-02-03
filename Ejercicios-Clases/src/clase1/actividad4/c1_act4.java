de2b63f6401692aff2f6d1c511fcb3f612019cb1
francoosanchez <francosanchezaguero@gmail.com>
2025-02-03 18:54:35 -0300

    clase 1 - actividad 4

--- Ejercicios-Clases/src/clase1/actividad4/c1_act4.java
+++ Ejercicios-Clases/src/clase1/actividad4/c1_act4.java
@@ -1,5 +1,7 @@
 package clase1.actividad4;
 
+import java.util.Scanner;
+
 /*
  Escribe un programa que:
 - Solicite un número entero n al usuario.
@@ -8,7 +10,18 @@ package clase1.actividad4;
 
 public class c1_act4 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número entero: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " es par");
+            } else {
+                System.out.println(i + " es impar");
+            }
+        }
+        scanner.close();
     }
     
 }
