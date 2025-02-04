1bcba19cae4582adff575075ea9fb9602f46b328
agusEmaUade <agyanez@uade.edu.ar>
2025-02-04 20:21:57 -0300

    feat: add clase2 actividad5

---
+++ untitled/src/main/java/org/example/clase2/Actividad5.java
@@ -0,0 +1,14 @@
+package org.example.clase2;
+
+public class Actividad5 {
+    public static int sumaRecursiva(int n) {
+        if (n <= 0) return 0;  // Caso base
+        return n + sumaRecursiva(n - 1); // Llamada recursiva
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println("La suma de los primeros " + n + " números es: " + sumaRecursiva(n));
+        // Salida esperada: 55
+    }
+}
