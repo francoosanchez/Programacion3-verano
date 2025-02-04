ccac0396891c25575967d585f7ce4ff70a9bad9b
agusEmaUade <agyanez@uade.edu.ar>
2025-02-04 19:51:01 -0300

    feat: clase1 actividad4

---
+++ untitled/src/main/java/org/example/clase1/Actividad4.java
@@ -0,0 +1,22 @@
+package org.example.clase1;
+
+import java.util.Scanner;
+
+public class Actividad4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número entero: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " es par");
+            } else {
+                System.out.println(i + " es impar");
+            }
+        }
+        scanner.close();
+    }
+
+}
