e04c40139c3d123c47c04ae48e85f5624389d437
francoosanchez <francosanchezaguero@gmail.com>
2025-02-10 15:18:59 -0300

    clase 7 - actividad 4

--- Ejercicios-Clases/src/clase7/actividad4/c7_act4.java
+++ Ejercicios-Clases/src/clase7/actividad4/c7_act4.java
@@ -21,5 +21,30 @@ package clase7.actividad4;
  */
 
 public class c7_act4 {
+    public static int seleccionPaquetes(int[] costos, int[] ganancia, int presupuesto)
+    {
+        int n = costos.length;
+        int[][] dp = new int[n + 1][presupuesto + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= presupuesto; j++) {
+                if (costos[i-1] <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - costos[i - 1]] + ganancia[i - 1]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][presupuesto];
+    }
+
+    public static void main(String[] args) 
+    {
+        int [] costos = {12, 20, 15, 25};
+        int [] ganancias = {150, 200, 100, 300};
+        int presupuesto = 35;
+
+        System.out.println("El beneficio máximo con el presupuesto " + presupuesto + " es: " + seleccionPaquetes(costos, ganancias, presupuesto));
+    }
     
 }
