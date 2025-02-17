1ccac4076205d5904257edd9abdfce8e87aaef4e
francoosanchez <francosanchezaguero@gmail.com>
2025-02-17 16:25:52 -0300

    clase 11 - act 1

---
+++ Ejercicios-Clases/src/clase11/actividad2/c11_act2.java
@@ -0,0 +1,16 @@
+package clase11.actividad2;
+
+/*
+    Actividad 2
+
+    En el diseño de interiores de oficinas o hogares, necesitamos un programa que
+    imprima las combinaciones posibles de la ubicación de escritorios y sillas, en una
+    habitación de 4x4. Las restricciones son: garantizar que ningún elemento esté en
+    la misma "fila" (por ejemplo, en una misma fila de escritorios) o en la misma
+    "columna" (por ejemplo, alineación en una pared) para optimizar el uso del
+    espacio y facilitar la circulación.
+ */
+
+public class c11_act2 {
+    
+}
---
+++ Ejercicios-Clases/src/clase11/actividad3/c11_act3.java
@@ -0,0 +1,21 @@
+package clase11.actividad3;
+
+/*
+    Actividad 3
+    
+    Diseño de Distribución de Equipos Electrónicos en Oficinas
+    En una oficina de 4x4, se necesita organizar la disposición de computadoras y
+    impresoras de manera que optimice el uso del espacio y facilite el acceso. Las
+    restricciones de diseño son las siguientes:
+    No puede haber dos computadoras en la misma fila o columna.
+    No puede haber dos impresoras en la misma fila o columna.
+    Debes encontrar todas las combinaciones posibles para colocar 4 computadoras y 4
+    impresoras en el tablero, respetando las restricciones anteriores.
+    Objetivo: Implementar un programa en Java que utilice la técnica de backtracking para
+    encontrar todas las configuraciones posibles de colocación de computadoras e
+    impresoras en el tablero de 4x4. Tu programa debe imprimir cada configuración válida.
+ */
+
+public class c11_act3 {
+    
+}
