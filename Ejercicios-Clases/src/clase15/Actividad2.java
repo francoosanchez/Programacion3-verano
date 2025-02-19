5e3bd70d3f355627e1ee296018da33d028b65dca
Facundo Carrizo <facundocarrizo99@gmail.com>
2025-02-19 19:15:08 -0300

    c15 act 2

---
+++ Ejercicios-Clases/src/clase15/Actividad2.java
@@ -0,0 +1,16 @@
+package clase15;
+
+public class Actividad2 {
+    /*
+    El problema del viajante consiste en encontrar la ruta más corta que visite una lista de ciudades exactamente una vez y regrese al punto de partida. Es un problema NP-Hard en optimización combinatoria.
+
+🔢 Combinaciones para 15 ciudades:
+El número de rutas posibles es ￼ considerando rutas simétricas.
+
+🔍 Métodos heurísticos destacados:
+	•	Heurísticas constructivas: generan soluciones iniciales rápidas.
+	•	Mejora iterativa: refinan soluciones mediante pequeñas modificaciones.
+	•	Mejoras aleatorias: incluyen algoritmos como colonia de hormigas.
+
+     */
+}
